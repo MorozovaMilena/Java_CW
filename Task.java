01e0e4acf6aa2e3401114fe9846751a59363f41c
MorozovaMilena <84088568+MorozovaMilena@users.noreply.github.com>
2023-12-13 14:54:16 +0300

    Update Task.java

--- Task.java
+++ Task.java
@@ -1,6 +1,11 @@
 public class PhoneBook {
     public static void main(String[] args) {
         HashMap<String, List<String>> phoneBook = new HashMap<>();
+        addContact(phoneBook, "Морозова", "123-456-789");
+        addContact(phoneBook, "Иванов", "987-654-321");
+        addContact(phoneBook, "Климов", "555-555-555");
+        addContact(phoneBook, "Морозова", "111-222-333");
+        addContact(phoneBook, "Петров", "444-444-444");
         printPhoneBook(phoneBook);
     }
 }
