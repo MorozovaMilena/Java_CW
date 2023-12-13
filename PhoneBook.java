7876ec181b3b4431924a95fe916d0a59ccd8ee62
MorozovaMilena <84088568+MorozovaMilena@users.noreply.github.com>
2023-12-13 14:52:41 +0300

    Update PhoneBook.java

--- PhoneBook.java
+++ PhoneBook.java
@@ -1,5 +1,20 @@
 public class PhoneBook {
 
-  String name;
-  String phone;
+ private String name;
+ private String phone;
+
+  public static void addContact(HashMap<String, List<String>> phoneBook, String name, String phone) {
+        List<String> phones = phoneBook.getOrDefault(name, new ArrayList<>());
+        phones.add(phone);
+        phoneBook.put(name, phones);
+    }
+
+    public static void printPhoneBook(HashMap<String, List<String>> phoneBook) {
+        List<Map.Entry<String, List<String>>> sortedList = new ArrayList<>(phoneBook.entrySet());
+        sortedList.sort((a, b) -> Integer.compare(b.getValue().size(), a.getValue().size()));
+
+        for (Map.Entry<String, List<String>> entry : sortedList) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
 }
